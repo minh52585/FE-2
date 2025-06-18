8dae0b0c641389b09991947fa1e6fc92e9350654
unknown <{minhpnph52585@gmail.com>
2025-06-18 10:46:23 +0700

    map

--- src/pages/Carts.tsx
+++
@@ -1,9 +0,0 @@
-const Carts = () => {
-  return (
-    <div>
-      carts-works!
-    </div>
-  )
-}
-
-export default Carts
--- src/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-import Product from '../components/common/product';
-
-const HomePage = () => {
-    return <Product />;
-};
-
-export default HomePage;
